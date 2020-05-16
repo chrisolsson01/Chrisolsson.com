9b0a04a200eae3353f3d980f58a8ffe0fee89b3f
Liam Olsson Lillieroth <liam.olssonlilliero@learnet.se>
2020-05-16 22:04:18 +0200

    .

--- assets/JavaScript/github.js
+++ assets/JavaScript/github.js
@@ -4,15 +4,45 @@ request.onload = function () {
   var data = JSON.parse(this.response);
   var statusHTML = '';
   var statusHTML2 = "";
+  var html;
+  var csharp;
+  var js;
+  var php;
+  var python;
+
   $.each(data, function(i, status) {
     //Graph
-    
-    statusHTML += $('#lang').append('<h3 style="color:black;" id="language">'+status.language+'</h3>');
+    // if()
     // $('#language')
+    if(status.language == "CSS"){
+      html += 1;
+    }
+    if(status.language == "TSQL"){
+      csharp += 1;
+    }
+    if(status.language == "PHP"){
+      php += 1;
+    }
+    if(status.language == "JavaScript"){
+      js += 1;
+    }
+    if(status.language == "Python"){
+      python += 1;
+    }
+    // if(status.language == "CSS"){
+    //   // if(!statusHTML == $('#lang').append('<h3 style="color:black;" id="language">HTML</h3>')){
+    //   //   statusHTML += $('#lang').append('<h3 style="color:black;" id="language">HTML</h3>');
+    //   // }
+    //   statusHTML += $('#lang').append('<h3 style="color:black;" id="language">HTML</h3>');
+    // }
+    // else if(status.language == "TSQL"){
+    //   statusHTML += $('#lang').append('<h3 style="color:black;" id="language">C#</h3>');
+    // }else{
+    //   statusHTML += $('#lang').append('<h3 style="color:black;" id="language">'+status.language+'</h3>');
+    // }
 
+    
 
-    //Feauturing projects
-    // statusHTML2 += $('#proj-features').append('<img src=""');
   });
   $('#horiGraph').html(statusHTML);
   
