73641f277d6a11c850b3002a094cec19a7ecee37
Liam Olsson Lillieroth <liam.olssonlilliero@learnet.se>
2020-05-22 18:27:19 +0200

    .

--- assets/JavaScript/feautured.js
+++
@@ -1,7 +0,0 @@
-$(".proj-feautures").hover(
-    function() {
-        $(this).append($("<div class='proj-feautures__hover'></div>"));
-    }, function() {
-        $(this).find("span").last().remove();
-    }
-)
