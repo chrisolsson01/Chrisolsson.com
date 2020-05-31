84bb5c3d9bb7ca832f583770ab614360298f08a0
Liam Olsson Lillieroth <liam.olssonlilliero@learnet.se>
2020-05-31 10:26:57 +0200

    Fix.

--- assets/JavaScript/github.js
+++ assets/JavaScript/github.js
@@ -4,49 +4,58 @@ request.onload = function () {
   var data = JSON.parse(this.response);
   var statusHTML = '';
   var statusHTML2 = "";
-  var html;
-  var csharp;
-  var js;
-  var php;
-  var python;
+  //Programming Lang. Counter
+  var html = 0;
+  var csharp = 0;
+  var js = 0;
+  var php = 0;
+  var python = 0;
 
   $.each(data, function(i, status) {
     //Graph
-    // if()
-    // $('#language')
+
+    /*   ---TODO---
+    * Increase #left_side width depending on how many projects.
+    * Example: if(html < 10 ) {width: 100px}
+    */
+
     if(status.language == "CSS"){
       html += 1;
-    }
-    if(status.language == "TSQL"){
-      csharp += 1;
+      statusHTML += $('#left_side').append('<p>'+html+'</p>');
     }
     if(status.language == "PHP"){
       php += 1;
+      statusHTML += $('#left_side__php').append('<p>'+php+'</p>');
+    }
+    if(status.language == "TSQL" || status.language == "C#"){
+      csharp += 1;
+      statusHTML += $('#left_side__csharp').append('<p>'+csharp+'</p>');
+      console.log(csharp);
     }
     if(status.language == "JavaScript"){
       js += 1;
+      statusHTML += $('#left_side__js').append('<p>'+js+'</p>');
     }
     if(status.language == "Python"){
       python += 1;
+      statusHTML += $('#left_side__python').append('<p>'+python+'</p>');
     }
-    // if(status.language == "CSS"){
-    //   // if(!statusHTML == $('#lang').append('<h3 style="color:black;" id="language">HTML</h3>')){
-    //   //   statusHTML += $('#lang').append('<h3 style="color:black;" id="language">HTML</h3>');
-    //   // }
-    //   statusHTML += $('#lang').append('<h3 style="color:black;" id="language">HTML</h3>');
-    // }
-    // else if(status.language == "TSQL"){
-    //   statusHTML += $('#lang').append('<h3 style="color:black;" id="language">C#</h3>');
-    // }else{
-    //   statusHTML += $('#lang').append('<h3 style="color:black;" id="language">'+status.language+'</h3>');
-    // }
-
-    
-
   });
+
+  //Store Programming Lang. Counter to json
+  var text = '{"employees":[' +
+  '{"firstName":"John","lastName":"Doe" },' +
+  '{"firstName":"Anna","lastName":"Smith" },' +
+  '{"firstName":"Peter","lastName":"Jones" }]}';
+
+
+  console.log("HTML: " + html + " PHP: " + php + " C#: " + csharp + " Javascript: " + js + " Python: " + python);
+  $('#lang_first').append('<h1>hej</h1>');
   $('#horiGraph').html(statusHTML);
   
+
 }
+
 request.send();
 
 $(function createGraph(){
